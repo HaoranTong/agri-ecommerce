d6ad0a58265ab28cdee78ec6e530074fd43a6899
HaoranTong <zhucethr@126.com>
2025-11-20 15:59:40 +0800

    feat: initial commit of MyShop Core plugin

---
+++ wp-content/plugins/myshop-core/myshop-core.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: MyShop Core
+ * Description: 微信小程序无头电商后端核心插件，提供 JWT 认证、自定义 API、分销、代理商、虚拟购物卡等功能。
+ * Version: 1.0.0
+ * Author: Your Team
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
+ * WC requires at least: 8.0
+ * WC tested up to: 9.3
+ * Plugin Type: woocommerce
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('MYSHOP_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+require_once MYSHOP_PLUGIN_DIR . 'includes/class-myshop-loader.php';
+
+MyShop_Loader::init();
