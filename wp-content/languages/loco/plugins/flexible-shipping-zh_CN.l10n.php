508dae0e209f9a0b635813ac857badba193aceaa
HaoranTong <zhucethr@126.com>
2025-12-03 18:10:06 +0800

    chore: 添加跟踪WordPress核心文件和插件，保证开发环境与生产环境一致

---
+++ wp-content/languages/loco/plugins/flexible-shipping-zh_CN.l10n.php
@@ -0,0 +1,3 @@
+<?php
+return ['project-id-version'=>'Flexible Shipping 6.4.1','report-msgid-bugs-to'=>'https://wordpress.org/support/plugin/flexible-shipping','last-translator'=>'','language-team'=>'简体中文','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2025-10-30T18:32:43+00:00','po-revision-date'=>'2025-11-15 00:12+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'flexible-shipping
+','language'=>'zh_CN','plural-forms'=>'nplurals=1; plural=0;','x-loco-version'=>'2.8.0; wp-6.8.3; php-8.3.26','messages'=>[]];
